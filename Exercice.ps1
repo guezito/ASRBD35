962b9b0cc4fb3f76048444746faa2e2b03803010
Najib <najib@zghrata.com>
2018-05-28 16:02:30 +0200

    first commit

---
+++ Exercice.ps1
@@ -0,0 +1,60 @@
+﻿function exo08
+{
+    for ($i =0; $i -lt 500; $i++) {
+    Write-Host -ForegroundColor Cyan "Je dois faire des sauvegardes régulierement"
+    }
+}
+
+
+function exo09
+{
+  for ($i =1; $i -lt 1500; $i= $i +2) {
+    Write-Host -ForegroundColor Yellow $i
+    }
+}
+
+
+
+
+function exo10
+{
+  for ($i = 13; $i -lt 131; $i= $i +13) {
+    Write-Host -ForegroundColor Red $i
+    }
+}
+
+
+function exo11
+{
+$test = Read-Host "Saisir un mot"
+$test.Length
+}
+
+
+function exo12
+{
+
+$u = Read-host "Saisir un nombre"
+if ($u % 2 -eq 0 ) {
+Write-Host -ForegroundColor Yellow "Ce nombre est pair"
+} else {
+Write-Host -ForegroundColor Yellow "Ce nombre est impair"
+}
+}
+
+
+function exo13
+{
+$u = Read-Host "Saisir un nombre compris entre 10 et 20"
+
+if ($u -ge 20) {
+Write-Host -ForegroundColor Yellow "Plus petit !"
+ }
+ if ($u -le 10) {
+ Write-Host -ForegroundColor Yellow "Plus Grand !"
+ }
+ if ($u -match 10 -and 20) {
+ Write-Host -ForegroundColor Yellow "C'est good"
+ } 
+}
+exo13
---
+++ Script.ps1
@@ -0,0 +1,44 @@
+﻿<#$chaine1 = "Hello "
+$chaine2 = "World :)"
+
+$chaine1 + $chaine2
+
+
+
+$feuvert = $true
+
+if ($feuvert) {
+    echo "J'avance";
+} else {
+    echo "Je m'arrête";
+    }
+
+    $fruits = @("Pomme" , "Cerise" , "Banane", "Poire", "Abricot")
+    
+    foreach ($fruit in $fruits) {
+        Write-Host -ForegroundColor Yellow $fruit;
+    }
+
+    for ($i=0; $i -lt 10; $i++) {
+    Write-Host -ForegroundColor Cyan $i
+    }
+
+    $i = 0;
+
+    while ($i -lt 10) {
+        Write-Host -ForegroundColor Red $i;
+        $i++;
+        }#>
+
+function disBonjour
+{
+    param ($prenom)
+    if ($prenom) {
+    echo "Bonjour $prenom"
+    } else {
+        echo "Il a pas dit Bonjour donc j'lui es niké ça mère"
+        }
+    }
+
+    #disBonjour("Michel")
+    disBonjour("")
